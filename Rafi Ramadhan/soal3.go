a1b3bae8c5d4cbca495285790717af7c516b5320
rafiramadhan <rafirama1020@gmail.com>
2025-12-03 19:46:03 +0700

    Create soal3.go

---
+++ Rafi Ramadhan/soal3.go
@@ -0,0 +1,15 @@
+ackage main
+
+import "fmt"
+
+func main() {
+    var x, y int
+    fmt.Print("Masukkan dua bilangan (x y): ")
+    fmt.Scan(&x, &y)
+
+    h := 0
+
+    for x >= y {
+        x = x - y   
+        h++   
+    }
